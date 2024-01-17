d6f3e266820a581cb9bc82d85687fac7651dd0f9
Guilherme Moraes <guilherme.moraes@outlook.com>
2024-01-17 12:47:22 -0800

    Remove legacy sw injection code

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('/sw.js')
-        .then((serviceWorker) => {
-            console.log('Service Worker registered: ', serviceWorker)
-        })
-        .catch((error) => {
-            console.error('Error registering the Service Worker: ', error)
-        })
-}
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <App />
--- vite.config.ts
+++ vite.config.ts
@@ -102,6 +102,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
       }
+
     },
     outDir: 'docs',
     assetsDir: '.'

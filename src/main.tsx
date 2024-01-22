e438337dd5cd074cf5b88476c3fda71c18c03a73
Guilherme Moraes <guilherme.moraes@outlook.com>
2024-01-21 17:29:03 -0800

    Remove pwa

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+navigator.serviceWorker.getRegistrations().then(function (registrations) {
+    for (let registration of registrations) {
+        registration.unregister();
+    }
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <App />
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,104 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa';
+// import { VitePWA } from 'vite-plugin-pwa';
+
+//devDependencies:
+// "@vite-pwa/assets-generator": "^0.2.3",
+//   "vite-plugin-pwa": "^0.17.4"
+
 
 // https://vitejs.dev/config/
-const viteConfig = {
-  plugins: [react(),],
+export default defineConfig({
+  plugins: [react()
+    //   ,VitePWA({
+    //   registerType: 'prompt',
+    //   selfDestroying: true,
+    //   injectRegister: 'auto',
+    //   includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+    //   workbox: {
+    //     globPatterns: ['**/*.{js,css,html,json}'],
+    //     runtimeCaching: [
+    //       {
+    //         urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/.*/i,
+    //         handler: 'CacheFirst',
+    //         options: {
+    //           cacheName: 'bootstrap-cache',
+    //           expiration: {
+    //             maxEntries: 10,
+    //             maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+    //           },
+    //           cacheableResponse: {
+    //             statuses: [0, 200]
+    //           }
+    //         }
+    //       },
+    //       {
+    //         urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+    //         handler: 'CacheFirst',
+    //         options: {
+    //           cacheName: 'google-fonts-cache',
+    //           expiration: {
+    //             maxEntries: 10,
+    //             maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+    //           },
+    //           cacheableResponse: {
+    //             statuses: [0, 200]
+    //           }
+    //         }
+    //       },
+    //       {
+    //         urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+    //         handler: 'CacheFirst',
+    //         options: {
+    //           cacheName: 'gstatic-fonts-cache',
+    //           expiration: {
+    //             maxEntries: 10,
+    //             maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+    //           },
+    //           cacheableResponse: {
+    //             statuses: [0, 200]
+    //           },
+    //         }
+    //       }
+    //     ]
+    //   },
+    //   strategies: 'generateSW',
+    //   manifest: {
+    //     name: 'Sudoku',
+    //     short_name: 'Sudoku',
+    //     description: 'Sudoku',
+    //     theme_color: '#8936FF',
+    //     background_color: '#2EC6FE',
+    //     orientation: 'any',
+    //     display: 'fullscreen',
+    //     lang: 'en-US',
+    //     icons: [
+    //       {
+    //         src: 'pwa-64x64.png',
+    //         sizes: '64x64',
+    //         type: 'image/png'
+    //       },
+    //       {
+    //         src: 'pwa-192x192.png',
+    //         sizes: '192x192',
+    //         type: 'image/png'
+    //       },
+    //       {
+    //         src: 'pwa-512x512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png',
+    //         purpose: 'any'
+    //       },
+    //       {
+    //         src: 'maskable-icon-512x512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png',
+    //         purpose: 'maskable'
+    //       }
+    //     ],
+    //   },
+    // })
+  ],
   base: '/sudoku',
   build: {
     target: 'esnext',
@@ -16,102 +110,5 @@ const viteConfig = {
     outDir: 'docs',
     assetsDir: '.'
   }
-}
-
-const pwaEnabled = false;
-
-if (pwaEnabled) {
-
-  const pwaConfig = VitePWA({
-    registerType: 'prompt',
-    injectRegister: 'auto',
-    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,json}'],
-      runtimeCaching: [
-        {
-          urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/.*/i,
-          handler: 'CacheFirst',
-          options: {
-            cacheName: 'bootstrap-cache',
-            expiration: {
-              maxEntries: 10,
-              maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
-            },
-            cacheableResponse: {
-              statuses: [0, 200]
-            }
-          }
-        },
-        {
-          urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-          handler: 'CacheFirst',
-          options: {
-            cacheName: 'google-fonts-cache',
-            expiration: {
-              maxEntries: 10,
-              maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
-            },
-            cacheableResponse: {
-              statuses: [0, 200]
-            }
-          }
-        },
-        {
-          urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
-          handler: 'CacheFirst',
-          options: {
-            cacheName: 'gstatic-fonts-cache',
-            expiration: {
-              maxEntries: 10,
-              maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
-            },
-            cacheableResponse: {
-              statuses: [0, 200]
-            },
-          }
-        }
-      ]
-    },
-    strategies: 'generateSW',
-    manifest: {
-      name: 'Sudoku',
-      short_name: 'Sudoku',
-      description: 'Sudoku',
-      theme_color: '#8936FF',
-      background_color: '#2EC6FE',
-      orientation: 'any',
-      display: 'fullscreen',
-      lang: 'en-US',
-      icons: [
-        {
-          src: 'pwa-64x64.png',
-          sizes: '64x64',
-          type: 'image/png'
-        },
-        {
-          src: 'pwa-192x192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: 'pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'any'
-        },
-        {
-          src: 'maskable-icon-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'maskable'
-        }
-      ],
-    },
-  })
-
-  viteConfig.plugins.push(pwaConfig)
-}
-
-export default defineConfig(viteConfig)
+})
 

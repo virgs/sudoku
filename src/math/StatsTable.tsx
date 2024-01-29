6c111aede7118ce11b4358d22168e3eaab4b4a28
Guilherme Moraes <guilherme.moraes@outlook.com>
2024-01-28 18:18:13 -0800

    Add best game

--- src/math/StatsTable.tsx
+++ src/math/StatsTable.tsx
@@ -1,12 +1,25 @@
 import { faLightbulb, faStopwatch, faTrophy, faX } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Database } from '../Database'
+import { Database, Stats } from '../Database'
 import { GameLevel, GameMode, modeLevelMap } from '../engine/types/AvailableGames'
 import { TimeFormatter } from '../time/TimeFormatter'
 import { NumberListOperations } from './NumberListOperations'
+import { useEffect } from 'react'
+import './StatsTable.css'
 
 export function StatsTable() {
+    useEffect(() => {
+        const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+        Array.from([...tooltipTriggerList]).map(
+            (tooltipTriggerEl) =>
+                //@ts-expect-error
+                new bootstrap.Tooltip(tooltipTriggerEl, {
+                    trigger: 'hover',
+                })
+        )
+    }, [])
     const databaseStats = Database.loadGameFinishedStats()
+
     const getModeStats = (mode: GameMode) => databaseStats.filter((stat) => stat.mode === mode)
     const getLatestTimeAModeWasFinished = (mode: GameMode) =>
         getModeStats(mode).reduce((acc, stat) => (stat.timestamp > acc ? stat.timestamp : acc), 0)
@@ -52,21 +65,43 @@ export function StatsTable() {
                             .get(mode)
                             ?.filter((level) => getLevelStats(level).length > 0)
                             .map((level) => {
+                                const levelStats = getLevelStats(level)
+                                const getBestTimeStats = () => {
+                                    const bestTimeStat = levelStats.reduce(
+                                        (acc, stat) => {
+                                            if (acc === undefined || stat.totalTime < acc.totalTime) {
+                                                return stat
+                                            }
+                                            return acc
+                                        },
+                                        undefined as Stats | undefined
+                                    )!
+                                    const time = new TimeFormatter().formatDuration(Math.trunc(bestTimeStat.totalTime))
+                                    const mistakes = bestTimeStat.mistakes
+                                    const hints = bestTimeStat.hints
+                                    return `<div class='tooltip-title'><h3>Best game</h3><span><h6>Time: ${time}</h6><h6>Mistakes: ${mistakes}</h6><h6>Hints: ${hints}</h6></div>`
+                                }
                                 return (
-                                    <tr key={mode + level}>
+                                    <tr
+                                        key={mode + level}
+                                        data-bs-toggle="tooltip"
+                                        data-bs-custom-class="sudoku-stats-tooltip"
+                                        data-bs-html="true"
+                                        data-bs-title={getBestTimeStats()}
+                                    >
                                         <th scope="row" style={{ textTransform: 'capitalize', width: '10%' }}>
                                             {level.toLowerCase()}
                                         </th>
-                                        <td>{getLevelStats(level).length || 0}</td>
+                                        <td>{levelStats.length || 0}</td>
                                         <td>{formatLevelAverageTime(level)}</td>
                                         <td>
                                             {NumberListOperations.getAverage(
-                                                getLevelStats(level).map((stat) => stat.mistakes)
+                                                levelStats.map((stat) => stat.mistakes)
                                             )?.toFixed(1) ?? '-'}
                                         </td>
                                         <td>
                                             {NumberListOperations.getAverage(
-                                                getLevelStats(level).map((stat) => stat.hints)
+                                                levelStats.map((stat) => stat.hints)
                                             )?.toFixed(1) ?? '-'}
                                         </td>
                                     </tr>
